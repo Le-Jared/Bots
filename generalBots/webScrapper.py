fbe2f3199aa76abbd3032552057c8929ac64caed
jared.lee <jared.lee.zhen.yan@gmail.com>
2023-07-13 10:44:05 +0800

    Update webScrapper.py
    - Add command-line interface
    - Add Rotating User Agents
    - IP Rotation/Proxies
    - Pagination

--- generalBots/webScrapper.py
+++ generalBots/webScrapper.py
@@ -1,14 +1,16 @@
+import argparse
 from bs4 import BeautifulSoup
 import requests
 import csv
 import os
 import time
+from fake_useragent import UserAgent
 
-def scrape_website(url):
+def scrape_website(url, proxies):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+        "User-Agent": ua.random
     }
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, proxies=proxies)
     
     if r.status_code != 200:
         print(f"Failed to retrieve page. Status code: {r.status_code}")
@@ -33,33 +35,46 @@ def clean_data(data):
     return ' '.join(data.split())
 
 def main():
-    base_url = "http://example.com"  # replace with the url you want to scrape
+    parser = argparse.ArgumentParser(description='Scrape a website.')
+    parser.add_argument('--url', type=str, help='The base URL to scrape')
+    args = parser.parse_args()
+
+    base_url = args.url
     filename = 'output.csv'
     check_file_exists(filename)
     
-    for i in range(1, 6):  # adjust range according to the number of pages you want to scrape
-        url = f"{base_url}/page/{i}"  
-        soup = scrape_website(url)
+    proxies = {
+        "http": "http://10.10.1.10:3128",
+        "https": "http://10.10.1.10:1080",
+    }
+
+    ua = UserAgent()
+
+    url = base_url
+    while url is not None:
+        soup = scrape_website(url, proxies)
         
         if soup is None:
             continue
 
-        # Assume each page has multiple items wrapped in div tags with class 'item'
         items = soup.find_all('div', attrs={'class': 'item'})
 
         for item in items:
             try:
-                # Assume each item has a 'title' in a h2 tag and 'description' in a p tag
                 title = clean_data(item.find('h2').text)
                 description = clean_data(item.find('p').text)
                 
-                # Write the data to a CSV file
                 write_to_file([title, description], filename)
                 
             except Exception as e:
                 print(f"Error when processing an item: {e}")
-        
-        # Delay between requests
+
+        next_page = soup.find('a', attrs={'class': 'next'})
+        if next_page is None:
+            break
+        else:
+            url = base_url + next_page.get('href')
+
         time.sleep(1)
 
 if __name__ == "__main__":
